be50268e5c54953371bcf256c7736024e01441ea
Aleksandra Niedzialkowska <aniedzialkowska2@gmail.com>
2023-06-28 16:36:59 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+{
+  const tasks = [
+
+  ];
+
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const removeTask = (taskIndex) => {
+    tasks.splice(taskIndex, 1);
+    render();
+  };
+
+  const toggleTaskDone = (index) => {
+    tasks[index].done = !tasks[index].done;
+    render();
+  };
+
+
+  const bindEvents = () => {
+    const removeButtons = document.querySelectorAll(".js-remove");
+
+    removeButtons.forEach((removeButton, taskIndex) => {
+      removeButton.addEventListener("click", () => {
+        removeTask(taskIndex);
+      });
+    });
+
+    const toggleDoneButtons = document.querySelectorAll(".js-done");
+    toggleDoneButtons.forEach((toggleDoneButton, index) => {
+      toggleDoneButton.addEventListener("click", () => {
+        toggleTaskDone(index);
+      });
+    });
+  };
+
+  const render = () => {
+    let htmlString = "";
+
+    for (const task of tasks) {
+      htmlString += `
+        <li class="list__item">
+          <button class="list__buttonTask list__buttonTask--toggleDone js-done">
+            ${task.done ? " ✓ " : ""}
+          </button>
+          <span class="list__content ${task.done ? "list__content--done" : ""}">
+            ${task.content}
+          </span>
+          <button class="list__buttonTask list__buttonTask--remove js-remove">
+            🗑
+          </button>
+        </li>
+      `;
+    }
+
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+
+    bindEvents();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskElement = document.querySelector(".js-newTask");
+    const newTaskContent = newTaskElement.value.trim();
+
+    if (newTaskContent !== "") {
+      addNewTask(newTaskContent);
+    }
+
+    newTaskElement.value = "";
+    newTaskElement.focus();
+  };
+
+  const init = () => {
+    render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
+  };
+
+  init();
+}
